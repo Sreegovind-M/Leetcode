a6f733eb8f46dd2fe9e45ddec531c307305eb480
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2024-04-02 23:02:43 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (11.69%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -12,7 +12,7 @@ class Solution {
             if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]){
                 res = i;
             }
-            else if (nums[i] < nums[i + 1]){
+            else if (nums[i + 1] > nums[i]){
                 res = i + 1;
             }
         }
