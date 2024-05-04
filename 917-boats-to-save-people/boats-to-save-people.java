3ac3dddb11686b453a3c81d52d6bd6e5f5d9d965
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2024-05-04 13:03:48 +0530

    Time: 15 ms (98.97%) | Memory: 54.6 MB (49.12%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,41 +1,17 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-
         int count = 0;
-        
-        int left = 0;
-        int right = people.length - 1;
 
-        while (left <= right){
-            if (people[right] == limit){
-                right--;
-                count++;
-            }
-            else {
-                int add = people[left] + people[right];
+        int l = 0, r = people.length - 1;
 
-                if (add == limit){
-                    count++;
-                    left++;
-                    right--;
-                }
-                else if (add > limit){
-                    right--;
-                    count++;
-                }
-                else {
-                    left++;
-                    right--;
-                    count++;
-                }
+        while (l <= r){
+            if ((people[l] + people[r]) <= limit){
+                l++;
             }
-            if (left == right && people[right] <= limit){
-                count++;
-                return count;
-            }
+            r--;
+            count++;
         }
-
         return count;
     }
 }
