ed89a0926ca6a100dd5ff3735bf071f96098dc87
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2024-05-14 06:00:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (45.29%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.java
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -1,35 +1,22 @@
 class Solution {
     public int matrixScore(int[][] grid) {
-        int ans = 0;
-        int n = grid.length;
+        int n = grid.length; 
         int m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            if (grid[i][0] == 0){
-                for (int j = 0; j < m; j++){
-                    grid[i][j] ^= 1;
-                }
-            }
-        }
+
+        int ans = n * (int)(Math.pow(2, m - 1 - 0));
 
         for (int j = 1; j < m; j++){
             int col_count = 0;
             for (int i = 0; i < n; i++){
-                col_count += grid[i][j];
+                col_count += grid[i][0] == 1 ? grid[i][j] : (grid[i][j] ^ 1);
             }
 
             if (col_count < (n - col_count)){
-                for (int i = 0; i < n; i++){
-                    grid[i][j] ^= 1;
-                }
+                ans += (n - col_count) * Math.pow(2, m - 1 - j);
             }
-        }
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                ans += grid[i][j] * Math.pow(2, m - 1 - j);
+            else {
+                ans += col_count * Math.pow(2, m - 1 - j);
             }
-                System.out.println(Arrays.toString(grid[i]));
-                
         }
         return ans;
     }
