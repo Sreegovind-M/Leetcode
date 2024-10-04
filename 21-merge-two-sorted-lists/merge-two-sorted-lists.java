0a0705ae7e22466118edc599828712479b6d29b7
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2024-10-04 09:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (34.82%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,22 +10,46 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode res = null;
-        if (list1 == null){
+        if (list1 == null && list2 == null){
+            return null;
+        }
+        else if (list1 == null && list2 != null){
             return list2;
         }
-        else if (list2 == null){
+        else if (list1 != null && list2 == null){
             return list1;
         }
-
-        if (list1.val <= list2.val){
-            res = list1;
-            res.next = mergeTwoLists(list1.next, list2);
-        }
         else {
-            res = list2;
-            res.next = mergeTwoLists(list1, list2.next);
+            ListNode res = new ListNode(0);
+            ListNode temp1 = list1;
+            ListNode temp2 = list2;
+            ListNode iterate = res;
+
+            while (temp1 != null && temp2 != null){
+                ListNode node1 = temp1;
+                ListNode node2 = temp2;
+                if (temp1.val <= temp2.val){
+                    iterate.next = node1;
+                    iterate = node1;
+                    temp1 = temp1.next;
+                }
+                else {
+                    iterate.next = node2;
+                    iterate = node2;
+                    temp2 = temp2.next;
+                }
+            }
+            if (temp1 != null){
+                ListNode node = temp1;
+                iterate.next = node;
+                temp1 = temp1.next;
+            }
+            if (temp2 != null){
+                ListNode node = temp2;
+                iterate.next = node;
+                temp2 = temp2.next;
+            }
+            return res.next;
         }
-        return res;
     }
 }
