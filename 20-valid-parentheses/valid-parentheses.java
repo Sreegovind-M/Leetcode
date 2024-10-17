e5f25cc3546a6eb9a532d4b2c4b7e2281942dabd
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2024-10-17 09:52:31 +0530

    Time: 2 ms (56.22%) | Memory: 41.8 MB (17.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,38 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for (char c : s.toCharArray()){
-            if (c == '(' || c == '[' || c == '{'){
-                st.push(c);
-            }
-            else {
-                if (st.empty()){
-                    return false;
-                }
-                else if (c == ')' && st.peek() == '('){
-                    st.pop();
-                }
-                else if (c == ']' && st.peek() == '['){
-                    st.pop();
-                }
-                else if (c == '}' && st.peek() == '{'){
-                    st.pop();
-                }
-                else {
-                    return false;
-                }
-            }
+        // for (char c : s.toCharArray()){
+        //     if (c == '(' || c == '[' || c == '{'){
+        //         st.push(c);
+        //     }
+        //     else {
+        //         if (st.empty()){
+        //             return false;
+        //         }
+        //         else if (c == ')' && st.peek() == '('){
+        //             st.pop();
+        //         }
+        //         else if (c == ']' && st.peek() == '['){
+        //             st.pop();
+        //         }
+        //         else if (c == '}' && st.peek() == '{'){
+        //             st.pop();
+        //         }
+        //         else {
+        //             return false;
+        //         }
+        //     }
             
+        // }
+        // return st.empty();
+        for(char c : s.toCharArray())
+        {
+            if(c == '(') st.push(')');
+            if(c == '[') st.push(']');
+            if(c == '{') st.push('}');
+            if(c == ']' || c == ')' || c == '}')
+                if(st.isEmpty() || st.pop() != c) return false;
         }
-        return st.empty();
+        return true && st.isEmpty();
     }
 }
