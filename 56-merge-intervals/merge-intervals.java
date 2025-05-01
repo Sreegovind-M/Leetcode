11addaefaebee61d432bcd43c750cdd92eaa89d9
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-05-01 10:20:04 +0530

    Time: 11 ms (13.25%) | Memory: 46.8 MB (25.74%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -5,27 +5,24 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         int n = intervals.length;
+        int x = intervals[0][0];
+        int y = intervals[0][1];
         for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(intervals[i]));
-        }
-        int i = 0;
-        while (i < n) {
-            int x = intervals[i][0];
-            int y = intervals[i][1];
-
-            int j = i + 1;
-            int temp = y;
-            while (j < n && intervals[j][0] <= temp) {
-                temp = Math.max(intervals[j][1], temp);
-                j++;
+            if (intervals[i][0] <= y) {
+                y = Math.max(intervals[i][1], y);
             }
-            System.out.println(i);
-            map.put(x, temp);
-            i = j;
+            else {
+                map.put(x, y);
+                x = intervals[i][0];
+                y = intervals[i][1];
+            }
+        }
+        if (!map.containsKey(x)) {
+            map.put(x, y);
         }
         int res[][] = new int[map.size()][2];
         
-        i = 0;
+        int i = 0;
         for (var m : map.entrySet()) {
             res[i][0] = m.getKey();
             res[i][1] = m.getValue();
