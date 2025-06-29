8bbb35185549839435025f4c33f01ed052bfbb92
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-06-29 17:00:37 +0530

    Time: 10 ms (22.04%) | Memory: 46.4 MB (84.31%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         int n = intervals.length;
         int x = intervals[0][0];
         int y = intervals[0][1];
-        for (int i = 0; i < n; i++) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i < n; i++) {
             if (intervals[i][0] <= y) {
-                y = Math.max(intervals[i][1], y);
+                y = Math.max(y, intervals[i][1]);
             }
             else {
                 map.put(x, y);
@@ -21,14 +21,12 @@ class Solution {
             map.put(x, y);
         }
         int res[][] = new int[map.size()][2];
-        
         int i = 0;
         for (var m : map.entrySet()) {
             res[i][0] = m.getKey();
             res[i][1] = m.getValue();
             i++;
         }
-
         return res;
     }
 }
