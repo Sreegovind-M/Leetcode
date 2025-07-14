94d39fe2b57b56e3e77511c1a9c19a4387a4bc73
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-07-14 19:08:45 +0530

    Time: 3 ms (91.34%) | Memory: 43.1 MB (75.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
+        char freq[] = new char[26];
 
-        for (char c : s.toCharArray()){
-            freq1[c - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for (char c : t.toCharArray()){
-            freq2[c - 'a']++;
-        }
-
-        for (int i = 0; i < 26; i++){
-            if (freq1[i] != freq2[i]){
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
                 return false;
             }
         }
