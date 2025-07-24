dfbd23ff474e8cff3180bbeee7c8757c7a77f1d5
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-07-24 22:33:59 +0530

    Time: 57 ms (1.58%) | Memory: 42.8 MB (65.22%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,16 +2,25 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
 
-        fun(nums, res, new ArrayList<>(), 0);
+        List<Integer> al = new ArrayList<>();
+
+        findSubsets(nums, 0, al, res);
+
         return res;
     }
-    public void fun(int nums[], List<List<Integer>> res, ArrayList<Integer> al, int ind){
-        res.add(new ArrayList<>(al));
-
-        for (int i = ind; i < nums.length; i++){
+    public void findSubsets(int nums[], int ind, List<Integer> al, List<List<Integer>> res) {
+        if (ind == nums.length) {
+            if (!res.contains(al)) {
+                res.add(new ArrayList<>(al));
+            }
+            return;
+        }
+        // res.add(new ArrayList<>(al));
+        for (int i = ind; i < nums.length; i++) {
             al.add(nums[i]);
-            fun(nums, res, al, i + 1);
+            findSubsets(nums, i + 1, al, res);
             al.remove(al.size() - 1);
         }
+        findSubsets(nums, ind + 1, al, res);
     }
 }
