9d2cab24696ad730259f1de31fd012ce8d55ae3e
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-07-28 21:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (58.94%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,35 +10,34 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return head;
-        }
-        ListNode reverse_head = reverse(head);
-
+        int count = 1;
+        head = reverse(head);
         if (n == 1) {
-            reverse_head = reverse_head.next;
-            return reverse(reverse_head);
+            head = head.next;
+            return reverse(head);
         }
-
-        int i = 0;
-        ListNode temp = reverse_head;
-        while (temp != null) {
-            i++;
-            if (i == n - 1) {
-                temp.next = temp.next.next;
-            }
+        ListNode temp = head;
+        while (temp != null && count + 1 < n) {
+            count++;
             temp = temp.next;
         }
-        return reverse(reverse_head);
-    }
-    public ListNode reverse(ListNode head) {
-        if (head == null || head.next == null) {
+        if (temp == null || temp.next == null) {
             return head;
         }
-        ListNode new_head = reverse(head.next);
-        ListNode next_node = head.next;
-        next_node.next = head;
-        head.next = null;
-        return new_head;
+        temp.next = temp.next.next;
+        head = reverse(head);
+        return head;
+    }
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
     }
 }
