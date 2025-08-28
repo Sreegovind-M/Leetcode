9e9e89b06adcfdab7cb624688db9f74523d079ca
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-08-28 07:05:08 +0530

    Time: 11 ms (7.40%) | Memory: 45.4 MB (8.25%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,63 +1,51 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char board[][] = new char[n][n];
         List<List<String>> res = new ArrayList<>();
 
-        n_queen(board, n, 0, res);
+        List<String> al = new ArrayList<>();
+        char[][] mat = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = '.';
+            }
+        }
+        find(n, mat, 0, res);
+
         return res;
     }
-
-    public void arr_list(char board[][], int n, List<List<String>> res){
-
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            String temp = "";
-            for (int j = 0; j < n; j++){
-                if (board[i][j] == 'Q'){
-                    temp += "Q";
-                }
-                else {
-                    temp += ".";
-                }
-            }
-            list.add(temp);
-        }
-        res.add(list);
-    }
-
-    public void n_queen(char board[][], int n, int col, List<List<String>> res){
-        if (col == n){
-            arr_list(board, n, res);
+    public void find(int n, char[][] mat, int col, List<List<String>> res) {
+        if (col == n) {
+            addList(mat, res);
             return;
         }
-        for (int i = 0; i < n; i++){
-            if (safe(board, i, col, n)){
-                board[i][col] = 'Q';
-                n_queen(board, n, col + 1, res);
-                board[i][col] = '.'; 
+        for (int i = 0; i < n; i++) {
+            if (isSafe(mat, i, col, n)) {
+                mat[i][col] = 'Q';
+                find(n, mat, col + 1, res);
+                mat[i][col] = '.';
             }
         }
     }
-    public boolean safe(char board[][], int x, int y, int n){
-        //Horizontal Left
-        for (int i = 0; i < y; i++){
-            if (board[x][i] == 'Q'){
-                return false;
+    public void addList(char[][] mat, List<List<String>> res) {
+        List<String> al = new ArrayList<>();
+        for (int i = 0; i < mat.length; i++) {
+            String s = "";
+            for (int j = 0; j < mat.length; j++) {
+                s += mat[i][j];
             }
+            al.add(s);
         }
-
-        //Left Down Diagonal
-        for (int i = x, j = y; i < n && j >= 0; i++, j--){
-            if (board[i][j] == 'Q'){
-                return false;
-            }
+        res.add(al);
+    }
+    public boolean isSafe(char[][] mat, int x, int y, int n) {
+        for (int i = 0; i < y; i++) {
+            if (mat[x][i] == 'Q') return false;
         }
-
-        //Left Up Diagonal
-        for (int i = x, j = y; i >= 0 && j >= 0; i--, j--){
-            if (board[i][j] == 'Q'){
-                return false;
-            }
+        for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+            if (mat[i][j] == 'Q') return false;
+        }
+        for (int i = x + 1, j = y - 1; i < n && j >= 0; i++, j--) {
+            if (mat[i][j] == 'Q') return false;
         }
         return true;
     }
