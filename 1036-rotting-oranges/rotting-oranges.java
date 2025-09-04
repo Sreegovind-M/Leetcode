a0f0c62448c8bf170ad2da8482343ec78ad7e0a9
Sreegovind <118262903+Sreegovind-M@users.noreply.github.com>
2025-09-04 22:16:07 +0530

    Time: 1 ms (100.00%) | Memory: 42 MB (97.22%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,67 +1,36 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> que = new LinkedList<>();
-        int n = grid.length;
-        int m = grid[0].length;
+        int count = 2;
 
-        int count_1 = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
-                    que.add(new Pair(i, j, 0));
-                }
-
-                if (grid[i][j] == 1) {
-                    count_1++;
+                    dfs(grid, visited, i, j, 2);
                 }
             }
         }
-        int change = 0;
-        int sec = 0;
-        if (count_1 == change) {
-            return sec;
-        }
-        while (!que.isEmpty()) {
-            Pair p = que.poll();
 
-            int x_co = p.x;
-            int y_co = p.y;
-            int tm = p.time;
-            sec = tm;
-            if (x_co - 1 >= 0 && grid[x_co - 1][y_co] == 1) {
-                change++;
-                grid[x_co - 1][y_co] = 2;
-                que.add(new Pair(x_co - 1, y_co, tm + 1));
-            }
-            if (x_co + 1 < n && grid[x_co + 1][y_co] == 1) {
-                change++;
-                grid[x_co + 1][y_co] = 2;
-                que.add(new Pair(x_co + 1, y_co, tm + 1));
-            }
-            if (y_co + 1 < m && grid[x_co][y_co + 1] == 1) {
-                change++;
-                grid[x_co][y_co + 1] = 2;
-                que.add(new Pair(x_co, y_co + 1, tm + 1));
-            }
-            if (y_co - 1 >= 0 && grid[x_co][y_co - 1] == 1) {
-                change++;
-                grid[x_co][y_co - 1] = 2;
-                que.add(new Pair(x_co, y_co - 1, tm + 1));
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) return -1;
+                count = Math.max(count, grid[i][j]);
             }
         }
-        if (change == count_1) {
-            return sec;
-        }
-        return -1;
+        return count - 2;
     }
-}
-class Pair {
-    int x;
-    int y;
-    int time;
-    Pair(int x, int y, int time) {
-        this.x = x;
-        this.y = y;
-        this.time = time;
+    public void dfs(int grid[][], boolean visited[][], int i, int j, int minute) {
+        if (i == grid.length || j == grid[0].length || i < 0 || j < 0) {
+            return;
+        }
+        if (grid[i][j] == 0 || (1 < grid[i][j] && grid[i][j] < minute)) return;
+
+        grid[i][j] = minute;
+        visited[i][j] = true;
+
+        dfs(grid, visited, i - 1, j, minute + 1);
+        dfs(grid, visited, i + 1, j, minute + 1);
+        dfs(grid, visited, i, j - 1, minute + 1);
+        dfs(grid, visited, i, j + 1, minute + 1);
     }
 }
